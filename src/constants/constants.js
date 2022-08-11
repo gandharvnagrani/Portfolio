5900cf86367ee93e9d2c4f2fede6e476ed652f07
gandharvnagrani <gandharv.nagrani01@gmail.com>
2022-08-10 23:15:38 -0400

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'A-Star Algorithm Visualizer (Python)',
+    description: "Using the PyGame library in Python, I created an algorithm visualizer for the famous A-star pathfinding algorithm. The program finds the shortest path from the starting node to the end node in the most efficient way possible.",
+      image: '/images/astar.gif',
+      tags: ['PyGame', 'Python', 'Algorithm', 'A-star pathfinding', 'Visualizer'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Stable Matching Problem (Java)',
+    description:"As part of my object-oriented programming course, I built a command-line application in Java that solves the stable matching problem using the Gale-Shapley algorithm. The algorithm takes in formatted datasets for suitors and receivers and matches them in an optimal way. A matching is stable when there is no pair (A, B) where both participants prefer each other to their matched partners.",
+    image: '/images/smp.gif',
+    tags: ['Java', 'Gale-Shapley Algorithm', 'Command-line Application'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Stock Market Analysis (Python)',
+    description: "Made use of pandas, numpy, matlotlib, and seaborn in the jupyter environment to carry out a data analysis and visualization project on 4 major tech stocks. Drew inferences through various types of graphs and charts. Implemented a Monte Carlo Simulation to predict future stock price. ",
+      image: '/images/data.png',
+      tags: ['pandas', 'numpy', 'seaborn', 'matplotlib'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Google Search Clone',
+    description: "Creating a google search clone using the GSearch API, HTML, TailwindCSS, and React.js.",
+    image: '/images/4.jpg',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
